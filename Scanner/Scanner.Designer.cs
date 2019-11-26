30ea3ab3235f71ae0a7801d168bbe0968c79d377
Jakub Kalina <jakub.kalina@codeteam.pl>
2019-11-26 07:48:15 +0100

    Initial commit

---
+++ Scanner/Scanner.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Scanner
+{
+    partial class Scanner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxScannedDocument = new System.Windows.Forms.PictureBox();
+            this.comboBoxAvailableScanners = new System.Windows.Forms.ComboBox();
+            this.buttonCheckForAvailableScanners = new System.Windows.Forms.Button();
+            this.buttonStartScanning = new System.Windows.Forms.Button();
+            this.buttonSelectFileDirectory = new System.Windows.Forms.Button();
+            this.buttonSetScannerOptions = new System.Windows.Forms.Button();
+            this.labelScanningMode = new System.Windows.Forms.Label();
+            this.labelScanningContrast = new System.Windows.Forms.Label();
+            this.labelResolution = new System.Windows.Forms.Label();
+            this.comboBoxScanningMode = new System.Windows.Forms.ComboBox();
+            this.textBoxScanningContrast = new System.Windows.Forms.TextBox();
+            this.textBoxScanningResolution = new System.Windows.Forms.TextBox();
+            this.trackBarScanningContrast = new System.Windows.Forms.TrackBar();
+            this.trackBarSetScanningResolution = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScannedDocument)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarScanningContrast)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSetScanningResolution)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxScannedDocument
+            // 
+            this.pictureBoxScannedDocument.Location = new System.Drawing.Point(761, 12);
+            this.pictureBoxScannedDocument.Name = "pictureBoxScannedDocument";
+            this.pictureBoxScannedDocument.Size = new System.Drawing.Size(630, 891);
+            this.pictureBoxScannedDocument.TabIndex = 0;
+            this.pictureBoxScannedDocument.TabStop = false;
+            // 
+            // comboBoxAvailableScanners
+            // 
+            this.comboBoxAvailableScanners.FormattingEnabled = true;
+            this.comboBoxAvailableScanners.Location = new System.Drawing.Point(12, 67);
+            this.comboBoxAvailableScanners.Name = "comboBoxAvailableScanners";
+            this.comboBoxAvailableScanners.Size = new System.Drawing.Size(301, 28);
+            this.comboBoxAvailableScanners.TabIndex = 1;
+            this.comboBoxAvailableScanners.SelectedIndexChanged += new System.EventHandler(this.comboBoxAvailableScanners_SelectedIndexChanged);
+            // 
+            // buttonCheckForAvailableScanners
+            // 
+            this.buttonCheckForAvailableScanners.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonCheckForAvailableScanners.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.buttonCheckForAvailableScanners.Location = new System.Drawing.Point(12, 12);
+            this.buttonCheckForAvailableScanners.Name = "buttonCheckForAvailableScanners";
+            this.buttonCheckForAvailableScanners.Size = new System.Drawing.Size(301, 39);
+            this.buttonCheckForAvailableScanners.TabIndex = 2;
+            this.buttonCheckForAvailableScanners.Text = "Wyszukaj skanery";
+            this.buttonCheckForAvailableScanners.UseVisualStyleBackColor = true;
+            this.buttonCheckForAvailableScanners.Click += new System.EventHandler(this.buttonCheckForAvailableScanners_Click);
+            // 
+            // buttonStartScanning
+            // 
+            this.buttonStartScanning.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStartScanning.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.buttonStartScanning.Location = new System.Drawing.Point(16, 886);
+            this.buttonStartScanning.Name = "buttonStartScanning";
+            this.buttonStartScanning.Size = new System.Drawing.Size(722, 87);
+            this.buttonStartScanning.TabIndex = 3;
+            this.buttonStartScanning.Text = "Rozpocznij skanowanie";
+            this.buttonStartScanning.UseVisualStyleBackColor = true;
+            this.buttonStartScanning.Click += new System.EventHandler(this.buttonStartScanning_Click);
+            // 
+            // buttonSelectFileDirectory
+            // 
+            this.buttonSelectFileDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonSelectFileDirectory.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.buttonSelectFileDirectory.Location = new System.Drawing.Point(940, 934);
+            this.buttonSelectFileDirectory.Name = "buttonSelectFileDirectory";
+            this.buttonSelectFileDirectory.Size = new System.Drawing.Size(301, 39);
+            this.buttonSelectFileDirectory.TabIndex = 4;
+            this.buttonSelectFileDirectory.Text = "Zapisz jako";
+            this.buttonSelectFileDirectory.UseVisualStyleBackColor = true;
+            // 
+            // buttonSetScannerOptions
+            // 
+            this.buttonSetScannerOptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonSetScannerOptions.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.buttonSetScannerOptions.Location = new System.Drawing.Point(204, 171);
+            this.buttonSetScannerOptions.Name = "buttonSetScannerOptions";
+            this.buttonSetScannerOptions.Size = new System.Drawing.Size(301, 39);
+            this.buttonSetScannerOptions.TabIndex = 6;
+            this.buttonSetScannerOptions.Text = "Ustawienia skanera";
+            this.buttonSetScannerOptions.UseVisualStyleBackColor = true;
+            this.buttonSetScannerOptions.Click += new System.EventHandler(this.buttonSetScannerOptions_Click);
+            // 
+            // labelScanningMode
+            // 
+            this.labelScanningMode.AutoSize = true;
+            this.labelScanningMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelScanningMode.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.labelScanningMode.Location = new System.Drawing.Point(198, 279);
+            this.labelScanningMode.Name = "labelScanningMode";
+            this.labelScanningMode.Size = new System.Drawing.Size(329, 32);
+            this.labelScanningMode.TabIndex = 7;
+            this.labelScanningMode.Text = "Wybierz tryb skanowania";
+            // 
+            // labelScanningContrast
+            // 
+            this.labelScanningContrast.AutoSize = true;
+            this.labelScanningContrast.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelScanningContrast.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.labelScanningContrast.Location = new System.Drawing.Point(176, 467);
+            this.labelScanningContrast.Name = "labelScanningContrast";
+            this.labelScanningContrast.Size = new System.Drawing.Size(121, 32);
+            this.labelScanningContrast.TabIndex = 8;
+            this.labelScanningContrast.Text = "Kontrast";
+            // 
+            // labelResolution
+            // 
+            this.labelResolution.AutoSize = true;
+            this.labelResolution.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelResolution.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.labelResolution.Location = new System.Drawing.Point(180, 695);
+            this.labelResolution.Name = "labelResolution";
+            this.labelResolution.Size = new System.Drawing.Size(197, 32);
+            this.labelResolution.TabIndex = 9;
+            this.labelResolution.Text = "Rozdzielczość";
+            // 
+            // comboBoxScanningMode
+            // 
+            this.comboBoxScanningMode.FormattingEnabled = true;
+            this.comboBoxScanningMode.Location = new System.Drawing.Point(204, 341);
+            this.comboBoxScanningMode.Name = "comboBoxScanningMode";
+            this.comboBoxScanningMode.Size = new System.Drawing.Size(323, 28);
+            this.comboBoxScanningMode.TabIndex = 10;
+            // 
+            // textBoxScanningContrast
+            // 
+            this.textBoxScanningContrast.Location = new System.Drawing.Point(383, 474);
+            this.textBoxScanningContrast.Name = "textBoxScanningContrast";
+            this.textBoxScanningContrast.Size = new System.Drawing.Size(163, 26);
+            this.textBoxScanningContrast.TabIndex = 11;
+            this.textBoxScanningContrast.TextChanged += new System.EventHandler(this.textBoxScanningContrast_TextChanged);
+            // 
+            // textBoxScanningResolution
+            // 
+            this.textBoxScanningResolution.Location = new System.Drawing.Point(383, 695);
+            this.textBoxScanningResolution.Name = "textBoxScanningResolution";
+            this.textBoxScanningResolution.Size = new System.Drawing.Size(163, 26);
+            this.textBoxScanningResolution.TabIndex = 12;
+            this.textBoxScanningResolution.TextChanged += new System.EventHandler(this.textBoxScanningResolution_TextChanged);
+            // 
+            // trackBarScanningContrast
+            // 
+            this.trackBarScanningContrast.LargeChange = 1;
+            this.trackBarScanningContrast.Location = new System.Drawing.Point(180, 523);
+            this.trackBarScanningContrast.Name = "trackBarScanningContrast";
+            this.trackBarScanningContrast.Size = new System.Drawing.Size(366, 69);
+            this.trackBarScanningContrast.TabIndex = 13;
+            this.trackBarScanningContrast.Scroll += new System.EventHandler(this.trackBarScanningContrast_Scroll);
+            // 
+            // trackBarSetScanningResolution
+            // 
+            this.trackBarSetScanningResolution.LargeChange = 10;
+            this.trackBarSetScanningResolution.Location = new System.Drawing.Point(184, 745);
+            this.trackBarSetScanningResolution.Maximum = 2400;
+            this.trackBarSetScanningResolution.Name = "trackBarSetScanningResolution";
+            this.trackBarSetScanningResolution.Size = new System.Drawing.Size(362, 69);
+            this.trackBarSetScanningResolution.TabIndex = 14;
+            this.trackBarSetScanningResolution.Scroll += new System.EventHandler(this.trackBarSetScanningResolution_Scroll);
+            // 
+            // Scanner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(1442, 1014);
+            this.Controls.Add(this.trackBarSetScanningResolution);
+            this.Controls.Add(this.trackBarScanningContrast);
+            this.Controls.Add(this.textBoxScanningResolution);
+            this.Controls.Add(this.textBoxScanningContrast);
+            this.Controls.Add(this.comboBoxScanningMode);
+            this.Controls.Add(this.labelResolution);
+            this.Controls.Add(this.labelScanningContrast);
+            this.Controls.Add(this.labelScanningMode);
+            this.Controls.Add(this.buttonSetScannerOptions);
+            this.Controls.Add(this.buttonSelectFileDirectory);
+            this.Controls.Add(this.buttonStartScanning);
+            this.Controls.Add(this.buttonCheckForAvailableScanners);
+            this.Controls.Add(this.comboBoxAvailableScanners);
+            this.Controls.Add(this.pictureBoxScannedDocument);
+            this.Name = "Scanner";
+            this.Text = "Scanner";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScannedDocument)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarScanningContrast)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSetScanningResolution)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxScannedDocument;
+        private System.Windows.Forms.ComboBox comboBoxAvailableScanners;
+        private System.Windows.Forms.Button buttonCheckForAvailableScanners;
+        private System.Windows.Forms.Button buttonStartScanning;
+        private System.Windows.Forms.Button buttonSelectFileDirectory;
+        private System.Windows.Forms.Button buttonSetScannerOptions;
+        private System.Windows.Forms.Label labelScanningMode;
+        private System.Windows.Forms.Label labelScanningContrast;
+        private System.Windows.Forms.Label labelResolution;
+        private System.Windows.Forms.ComboBox comboBoxScanningMode;
+        private System.Windows.Forms.TextBox textBoxScanningContrast;
+        private System.Windows.Forms.TextBox textBoxScanningResolution;
+        private System.Windows.Forms.TrackBar trackBarScanningContrast;
+        private System.Windows.Forms.TrackBar trackBarSetScanningResolution;
+    }
+}
+
---
+++ Scanner/Scanner.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WIA;
+using System.IO;
+
+
+namespace Scanner
+{
+    public partial class Scanner : Form
+    {
+        /// <summary>
+        /// Menager urządzeń
+        /// </summary>
+        DeviceManager deviceManager = new DeviceManager();
+
+        /// <summary>
+        /// Podłączone urządzenie
+        /// </summary>
+        private DeviceInfo firstScannerAvailable;
+
+        /// <summary>
+        /// Kontrast
+        /// </summary>
+        private int contrast;
+
+        /// <summary>
+        /// Rozdzielczość
+        /// </summary>
+        private int resolution;
+
+        /// <summary>
+        /// Id wybranego skanera
+        /// </summary>
+        private int chosenDeviceId;
+
+        /// <summary>
+        /// Tryb skanowania
+        /// </summary>
+        private int colorMode;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public Scanner()
+        {
+            InitializeComponent();
+
+            // Dodanie opcji skanowania
+            comboBoxScanningMode.Items.Add("Kolorowe");
+            comboBoxScanningMode.Items.Add("Czarno białe");
+            comboBoxScanningMode.Items.Add("Szare");
+
+        }
+
+        /// <summary>
+        /// Wyszukanie wszystkich dostępnych skanerów
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCheckForAvailableScanners_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 1; i <= deviceManager.DeviceInfos.Count; i++)
+            {
+                var devices = deviceManager.DeviceInfos[i];
+
+                if (deviceManager.DeviceInfos[i].Type == WiaDeviceType.ScannerDeviceType)
+                {
+                    var device = deviceManager.DeviceInfos[i];
+                    comboBoxAvailableScanners.Items.Add(deviceManager.DeviceInfos[i].Properties["Name"].get_Value());
+                    firstScannerAvailable = deviceManager.DeviceInfos[i];
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Zmiana kontrastu przez użytkownika
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxScanningContrast_TextChanged(object sender, EventArgs e)
+        {
+            if(!Int32.TryParse(textBoxScanningContrast.Text, out contrast))
+            {
+                MessageBox.Show("Wprowadzono nieprawidłowy kontrast, spróbuj ponownie!");
+            }
+            else
+            {
+                trackBarScanningContrast.Value = contrast;
+                trackBarScanningContrast.Update();
+            }
+        }
+
+        /// <summary>
+        /// Zmiana rozdzielczości przez użytkownika
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxScanningResolution_TextChanged(object sender, EventArgs e)
+        {
+            if (!Int32.TryParse(textBoxScanningResolution.Text, out resolution))
+            {
+                MessageBox.Show("Wprowadzono nieprawidłową rozdzielczość, spróbuj ponownie!");
+            }
+            else
+            {
+                trackBarSetScanningResolution.Value = resolution;
+                trackBarSetScanningResolution.Update();
+            }
+        }
+
+        /// <summary>
+        /// Otwarcie okna z ustawieniami skanera dostępnych z zewnętrznaj biblioteki
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSetScannerOptions_Click(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Zmiana kontrastu suwakiem
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void trackBarScanningContrast_Scroll(object sender, EventArgs e)
+        {
+            contrast = trackBarScanningContrast.Value;
+            textBoxScanningContrast.Text = contrast.ToString();
+            textBoxScanningContrast.Update();
+        }
+
+        /// <summary>
+        /// Zmiana rozdzielczości suwakiem
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void trackBarSetScanningResolution_Scroll(object sender, EventArgs e)
+        {
+            resolution = trackBarSetScanningResolution.Value;
+            textBoxScanningResolution.Text = resolution.ToString();
+            textBoxScanningResolution.Update();
+        }
+
+
+
+        /// <summary>
+        /// Adjusts the settings of the scanner with the providen parameters.
+        /// </summary>
+        /// <param name="scannnerItem">Scanner Item</param>
+        /// <param name="scanResolutionDPI">Provide the DPI resolution that should be used e.g 150</param>
+        /// <param name="scanStartLeftPixel"></param>
+        /// <param name="scanStartTopPixel"></param>
+        /// <param name="scanWidthPixels"></param>
+        /// <param name="scanHeightPixels"></param>
+        /// <param name="brightnessPercents"></param>
+        /// <param name="contrastPercents">Modify the contrast percent</param>
+        /// <param name="colorMode">Set the color mode</param>
+        private static void AdjustScannerSettings(IItem scannnerItem, int scanResolutionDPI, int scanStartLeftPixel, int scanStartTopPixel, int scanWidthPixels, int scanHeightPixels, int brightnessPercents, int contrastPercents, int colorMode)
+        {
+            const string WIA_SCAN_COLOR_MODE = "6146";
+            const string WIA_HORIZONTAL_SCAN_RESOLUTION_DPI = "6147";
+            const string WIA_VERTICAL_SCAN_RESOLUTION_DPI = "6148";
+            const string WIA_HORIZONTAL_SCAN_START_PIXEL = "6149";
+            const string WIA_VERTICAL_SCAN_START_PIXEL = "6150";
+            const string WIA_HORIZONTAL_SCAN_SIZE_PIXELS = "6151";
+            const string WIA_VERTICAL_SCAN_SIZE_PIXELS = "6152";
+            const string WIA_SCAN_BRIGHTNESS_PERCENTS = "6154";
+            const string WIA_SCAN_CONTRAST_PERCENTS = "6155";
+            SetWIAProperty(scannnerItem.Properties, WIA_HORIZONTAL_SCAN_RESOLUTION_DPI, scanResolutionDPI);
+            SetWIAProperty(scannnerItem.Properties, WIA_VERTICAL_SCAN_RESOLUTION_DPI, scanResolutionDPI);
+            SetWIAProperty(scannnerItem.Properties, WIA_HORIZONTAL_SCAN_START_PIXEL, scanStartLeftPixel);
+            SetWIAProperty(scannnerItem.Properties, WIA_VERTICAL_SCAN_START_PIXEL, scanStartTopPixel);
+            SetWIAProperty(scannnerItem.Properties, WIA_HORIZONTAL_SCAN_SIZE_PIXELS, scanWidthPixels);
+            SetWIAProperty(scannnerItem.Properties, WIA_VERTICAL_SCAN_SIZE_PIXELS, scanHeightPixels);
+            SetWIAProperty(scannnerItem.Properties, WIA_SCAN_BRIGHTNESS_PERCENTS, brightnessPercents);
+            SetWIAProperty(scannnerItem.Properties, WIA_SCAN_CONTRAST_PERCENTS, contrastPercents);
+            SetWIAProperty(scannnerItem.Properties, WIA_SCAN_COLOR_MODE, colorMode);
+        }
+
+        /// <summary>
+        /// Modify a WIA property
+        /// </summary>
+        /// <param name="properties"></param>
+        /// <param name="propName"></param>
+        /// <param name="propValue"></param>
+        private static void SetWIAProperty(IProperties properties, object propName, object propValue)
+        {
+            Property prop = properties.get_Item(ref propName);
+            prop.set_Value(ref propValue);
+        }
+
+        /// <summary>
+        /// Rozpoczęcie skanowania
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonStartScanning_Click(object sender, EventArgs e)
+        {
+           // firstScannerAvailable = deviceManager.Devices.GetDevice(chosenDeviceId);
+
+        }
+
+
+        /// <summary>
+        /// Wybranie urządzenia z listy
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBoxAvailableScanners_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            chosenDeviceId = comboBoxAvailableScanners.SelectedIndex;
+        }
+    }
+}
